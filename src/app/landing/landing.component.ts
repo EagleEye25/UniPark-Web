035904f9b67098ea110981e39729adfb96f408c7
Jason Eybers <jasonheybers@gmail.com>
2018-04-03 12:12:31 +0200

    added info to console for observation

--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -12,7 +12,7 @@ import { filter } from 'rxjs/operators';
 export class LandingComponent implements OnInit {
   facilityNo: string;
   userPass: string;
-  userinfo: string;
+  
   // Imports login-dialog for use for modal
   loginDialogRef: MatDialogRef<LoginDialogComponent>;
 
@@ -27,7 +27,12 @@ export class LandingComponent implements OnInit {
     });
 
     this.loginDialogRef.afterClosed().subscribe(
-      data => console.log('Dialog output:', data)
+      data => {
+        console.log('Dialog output:', data);
+        this.facilityNo = data.facilityNo;
+        this.userPass = data.userPass;
+        console.log('doedoe: ', this.facilityNo, '  ', this.userPass)
+      }
   );
   }
 }
