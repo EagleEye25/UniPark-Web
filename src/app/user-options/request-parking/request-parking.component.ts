7fa79d32688420e08f85d9e0c1949a07fd3f3353
Jason Eybers <jasonheybers@gmail.com>
2018-07-20 10:21:15 +0200

    trying implementation of maps

--- src/app/user-options/request-parking/request-parking.component.ts
+++ src/app/user-options/request-parking/request-parking.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit, HostListener } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
-import { FormGroup, FormBuilder } from '@angular/forms';
+import { FormGroup, FormBuilder, FormControl } from '@angular/forms';
 import { MatSnackBarModule, MatSnackBar } from '@angular/material/snack-bar';
+import { mapboxgl } from 'mapbox-gl';
 
 import {AppService, BASE_URL} from '../../app.service';
 
@@ -15,8 +16,14 @@ export class RequestParkingComponent implements OnInit {
 
   requestOptions: any;
   form: FormGroup;
-  parkingArea: string;
+  parkingArea: any;
   parkingSpot: string;
+  selectedArea: string;
+  selectedSpot: any;
+  disableSelect = new FormControl(true);
+
+  distinctArea: any;
+  areaObj: {[param: string]: any} = {};
 
   constructor(
     private fb: FormBuilder,
@@ -28,7 +35,7 @@ export class RequestParkingComponent implements OnInit {
   ) { }
 
   // Mock data for dialog, implimentations for database
-  areas = [
+  areasU = [
     {value: 'area01', viewValue: 'North'},
     {value: 'area02', viewValue: 'South'},
     {value: 'area03', viewValue: 'West'}
@@ -40,13 +47,43 @@ export class RequestParkingComponent implements OnInit {
     {value: 'spots03', viewValue: 'A003'}
   ];
 
+  areas: any;
+
   ngOnInit() {
     this.form = this.fb.group({
       parkingArea: [this.parkingArea, []],
       parkingSpot: [this.parkingSpot, []]
     });
-    this.http.get(`${BASE_URL}/parking/assigned/` + this.appService.getState('FacilityID'))
-    .subscribe((response: any) => this.requestOptions = response);
+    this.http.get(`${BASE_URL}/parking/request/info/` + this.appService.getState('FacilityID'))
+    .subscribe((response: any) => { this.requestOptions = response;
+      // Gets distinct parking areas for displaying in select
+      this.distinctArea = Array.from(new Set(this.requestOptions
+        .map(area => area.ParkingArea)
+      ));
+
+      this.areaObj.Area = <ParkingRequest>(this.requestOptions);
+      // this.distinctArea = <ParkingRequest>(this.distinctArea);
+      /*
+      for (let k = 0; k < this.distinctArea.length; k++) {
+        this.objTest.Area = this.distinctArea[k];
+      }*/
+
+      /*
+        for (let k = 0; k < this.requestOptions.length; k++) {
+      }
+      */
+     /*for (let k = 0; k < this.distinctArea.length; k++) {
+      const createObject = {
+        id: k.toString(),
+        name: this.distinctArea[k]
+      };
+      this.areas.push(createObject);
+     }*/
+      console.log('working? ' + this.areas);
+      console.log(this.areaObj);
+      console.log('distinct: ' + this.distinctArea);
+      console.log(this.requestOptions);
+    });
   }
 
   // Aquires the parking data from dialog
@@ -66,6 +103,13 @@ export class RequestParkingComponent implements OnInit {
     this.dialogRef.close();
   }
 
+  getAreaFromSelect(index: number) {
+    // this.selectedArea = this.form.value.parkingArea;
+    console.log('selected: ' + this.parkingArea.ParkingArea);
+    console.log('test: ' + index);
+    this.disableSelect = new FormControl(false);
+  }
+
   // Captures keyboard events
   @HostListener('window:keydown', ['$event'])
     enterKeyEvent(event: any) {
@@ -78,4 +122,13 @@ export class RequestParkingComponent implements OnInit {
         break;
       }
     }
+}
+
+export interface ParkingRequest {
+  ParkingArea: string;
+  ParkingSpace: number;
+}
+export interface Test {
+  value: string;
+  viewValue: string;
 }
